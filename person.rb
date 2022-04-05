b4f66e563509e93566b45989bf9f4e9682c52899
andres-condezo <ra.condezomonge@gmail.com>
2022-04-05 01:51:50 -0500

    Create Person class

---
+++ person.rb
@@ -0,0 +1,23 @@
+# Person: A class to define attributes and behaviors
+class Person
+  # Constructor to initialize
+  def initialize(age, name = 'Unknown', parent_permission = 'true')
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+
+  # accessor get and set method
+  attr_accessor :name, :age
+  attr_reader :id
+
+  def can_use_services?
+    of_age? == true || @parent_permission
+  end
+
+  private
+
+  def of_age?
+    @age >= 18
+  end
+end
